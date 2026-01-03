c95ee91e599900201ae07baf448fab0dda8fdd14
Adam Fučík <155400992+bababooex@users.noreply.github.com>
2026-01-03 10:21:23 +0100

    Add files via upload

---
+++ app/menu.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+#Script uses whiptail to create "user friendly" interface
+# === File configuration and size ===
+TV_SCRIPT="./testrpitx"
+IMAGE_DIR="./images"
+VIDEO_DIR="./videos"
+TEMP_DIR="/tmp/tv_image.gray"
+# should be left alone, otherwise will not be really stable and make errors
+WIDTH=52
+HEIGHT=625
+# ===================================
+
+if [[ $EUID -ne 0 ]]; then
+  whiptail --msgbox "This program must be run as sudo!" 10 50
+  exit 1
+fi
+
+if ! command -v magick &>/dev/null; then
+  echo "Missing dependency: ImageMagick"
+  exit 1
+elif ! command -v ffmpeg &>/dev/null; then
+  echo "Missing dependency: ffmpeg"
+  exit 1 
+fi
+
+whiptail --msgbox "This program can cause interference!\nUse with caution!" 10 50
+
+while true; do
+  CHOICE=$(whiptail --title "Experimental TV transmitter" \
+    --menu "Select an option:" 20 60 10 \
+    "1" "Transmit test pattern" \
+    "2" "Transmit grayscale image" \
+    "3" "Transmit grayscale video" \
+    "4" "Exit" \
+    3>&1 1>&2 2>&3)
+
+  case "$CHOICE" in
+
+  "1")
+      FREQ=$(whiptail --inputbox \
+      "Enter transmit frequency (Hz):" \
+      10 50 "194000000" \
+      3>&1 1>&2 2>&3)
+
+      if [ -z "$FREQ" ]; then
+        whiptail --msgbox "Going back to menu!" 10 50
+        continue
+      fi
+        whiptail --msgbox "Transmitting test pattern!\nPress Ctrl+C to stop." 10 50
+        "$TV_SCRIPT" --pattern --freq "$FREQ" || whiptail --msgbox "Error running transmitter script."
+        whiptail --msgbox "Going back to menu!" 10 50
+      ;;
+  "2")
+    if [[ ! -d "$IMAGE_DIR" ]]; then
+      whiptail --msgbox "Image directory not found: $IMAGE_DIR" 10 50
+      continue
+    fi
+
+    IMAGE_MENU=""
+    for f in "$IMAGE_DIR"/*.{png,jpg,jpeg,bmp}; do
+      [[ -e "$f" ]] || continue
+      b=$(basename "$f")
+      IMAGE_MENU+=" $b $b"
+    done
+
+    if [[ -z "$IMAGE_MENU" ]]; then
+      whiptail --msgbox "No image files found." 10 50
+      continue
+    fi
+
+    IMAGE=$(whiptail --title "Select Image" \
+      --menu "Choose image to transmit:" 20 60 10 \
+      $IMAGE_MENU 3>&1 1>&2 2>&3)
+
+    [[ -z "$IMAGE" ]] && continue
+
+    magick convert "$IMAGE_DIR/$IMAGE" \
+      -resize ${WIDTH}x${HEIGHT}! \
+      -colorspace Gray \
+      -depth 8 \
+      "$TEMP_DIR"
+
+    FREQ=$(whiptail --inputbox \
+      "Enter transmit frequency (Hz):" \
+      10 50 "194000000" \
+      3>&1 1>&2 2>&3)
+
+    if [ -z "$FREQ" ]; then
+        whiptail --msgbox "Going back to menu!" 10 50
+        continue
+    fi
+       whiptail --msgbox "Transmitting image!\nPress Ctrl+C to stop." 10 50
+       "$TV_SCRIPT" --image "$TEMP_DIR" --freq "$FREQ" || whiptail --msgbox "Error running transmitter script."
+       rm -f /tmp/tv_image.gray
+       whiptail --msgbox "Going back to menu!" 10 50
+      ;;
+  "3")
+    if [[ ! -d "$VIDEO_DIR" ]]; then
+      whiptail --msgbox "Video directory not found: $VIDEO_DIR" 10 50
+      continue
+    fi
+
+    VIDEO_MENU=""
+    for f in "$VIDEO_DIR"/*.mp4; do
+      [[ -e "$f" ]] || continue
+      b=$(basename "$f")
+      VIDEO_MENU+=" $b $b"
+    done
+
+    if [[ -z "$VIDEO_MENU" ]]; then
+      whiptail --msgbox "No video files found." 10 50
+      continue
+    fi
+
+    VIDEO=$(whiptail --title "Select Video" \
+      --menu "Choose video to transmit:" 20 60 10 \
+      $VIDEO_MENU 3>&1 1>&2 2>&3)
+
+    [[ -z "$VIDEO" ]] && continue
+
+    FREQ=$(whiptail --inputbox \
+      "Enter transmit frequency (Hz):" \
+      10 50 "194000000" \
+      3>&1 1>&2 2>&3)
+
+
+    if [ -z "$FREQ" ]; then
+        whiptail --msgbox "Going back to menu!" 10 50
+        continue
+    fi
+       whiptail --msgbox "Transmitting video!\nPress Ctrl+C to stop." 10 50
+
+    ffmpeg -i "$VIDEO_DIR/$VIDEO" \
+      -vf scale=${WIDTH}:${HEIGHT},format=gray \
+      -pix_fmt gray \
+      -f rawvideo pipe:1 | "$TV_SCRIPT" --video - --freq "$FREQ" || whiptail --msgbox "Error running transmitter script."
+
+       whiptail --msgbox "Going back to menu!" 10 50
+      ;;
+    "4")
+      whiptail --msgbox "Good bye!" 10 50
+      break
+      ;;
+
+    *)
+      whiptail --msgbox "Good bye!" 10 50
+      break
+      ;;
+  esac
+done
---
+++ app/testrpitx.cpp
@@ -0,0 +1,138 @@
+#include "../src/librpitx.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <cstdint>
+//runnig bool, basically wide shutdown
+bool running = true;
+
+//using it here
+static void terminate(int)
+{
+    running = false;
+    fprintf(stderr, "\nStopping transmission...\n");
+}
+//generate B/W test pattern, for starters
+void generate_test_pattern(uint8_t *buf, int width, int height)
+{
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            buf[y * width + x] = (uint8_t)(255.0 * x / (width - 1));
+        }
+    }
+}
+void tv_transmitter(uint64_t freq, int mode, const char *path)
+{
+    const int SR = 1000000;
+    //frame size, not high qualit, but somehow stable
+    const int WIDTH = 52;
+    const int HEIGHT = 625;
+    const int FRAME_SIZE = WIDTH * HEIGHT;
+
+    uint8_t frame[FRAME_SIZE];
+
+    atv atvtest(freq, SR, 14, HEIGHT);
+    atvtest.start();
+
+    int fd = -1;
+    //input selection - test / 8 bit grayscale image / 8 bit grayscale video
+    if (mode == 0) // pattern
+    {
+        generate_test_pattern(frame, WIDTH, HEIGHT);
+    }
+    else if (mode == 1) //image
+    {
+        fd = open(path, O_RDONLY);
+        if (fd < 0)
+        {
+            perror("open");
+            return;
+        }
+
+        ssize_t r = read(fd, frame, FRAME_SIZE);
+        close(fd);
+
+        if (r != FRAME_SIZE)
+        {
+            fprintf(stderr, "Incorrect image size (%ld bytes)\n", r);
+            return;
+        }
+    }
+    else if (mode == 2) //video
+    {
+        fd = STDIN_FILENO;
+    }
+
+    while (running)//handle running at 25 fps
+    {
+        if (mode == 0 || mode == 1)
+        {
+            atvtest.SetFrame(frame, HEIGHT);
+            usleep(40000); //25fps
+        }
+        else if (mode == 2)
+        {
+            ssize_t r = read(fd, frame, FRAME_SIZE);
+            if (r <= 0)
+                break;
+            atvtest.SetFrame(frame, HEIGHT);
+        }
+    }
+}
+//main
+int main(int argc, char *argv[])
+{
+    uint64_t freq = 194000000; //default frequency set by author, based on divider setting etc and works best
+    int mode = -1;
+    const char *path = nullptr;
+    //arg parser, set by user, sets mode and frequency
+    for (int i = 1; i < argc; i++)
+    {
+        if (!strcmp(argv[i], "--freq") && i + 1 < argc)
+        {
+            freq = strtoull(argv[++i], nullptr, 10);
+        }
+        else if (!strcmp(argv[i], "--pattern"))
+        {
+            mode = 0;
+        }
+        else if (!strcmp(argv[i], "--image") && i + 1 < argc)
+        {
+            mode = 1;
+            path = argv[++i];
+        }
+        else if (!strcmp(argv[i], "--video") && i + 1 < argc)
+        {
+            mode = 2;
+            path = argv[++i];
+        }
+    }
+
+    if (mode == -1)
+    {
+        fprintf(stderr,
+            "Usage:\n"
+            "  tv_tx --pattern [--freq Hz]\n"
+            "  tv_tx --image file.gray [--freq Hz]\n"
+            "  tv_tx --video - [--freq Hz]\n");
+        return 1;
+    }
+
+    struct sigaction sa{};
+    sa.sa_handler = terminate;
+    sigaction(SIGINT,  &sa, nullptr);
+    sigaction(SIGTERM, &sa, nullptr);
+    sigaction(SIGQUIT, &sa, nullptr);
+
+    dbg_setlevel(1);
+
+    fprintf(stderr, "TV TX @ %llu Hz\n", freq);
+    tv_transmitter(freq, mode, path);
+
+    return 0;
+}
